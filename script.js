d0a00eff6d8e54a76e6a93540314f120e346ac51
jacob boutin <jakobboutin@jacobs-MacBook-Pro.local>
2017-07-10 18:34:14 -0600

    we added js tp cards updated stylesheet and html

--- script.js
+++ script.js
@@ -7,21 +7,29 @@ var deleteCardButton = document.querySelectorAll(".delete-button")
 
 enterButton.addEventListener("click", function(event){
   event.preventDefault();
+  addBookMark();
   console.log(inputTitle.value);
   console.log(inputUrl.value);
 });
 //HOW TO RETURN SOMTHING BACK USING THE QUERY SELCTOR ALL
-readCardButton.addEventListener("click", function(event){
-  event.preventDefault();
-});
+// readCardButton.addEventListener("click", function(event){
+//   event.preventDefault();
+// });
 
-deleteCardButton.addEventListener("click", function(event){
-  event.preventDefault();
-});
-function UrlCard(title, url){
-  this.title = title
-  this.url = url
-  return 
-};
+// deleteCardButton.addEventListener("click", function(event){
+//   event.preventDefault();
+// });
+function addBookMark(){
+  var title = inputTitle.value;
+  var url = inputUrl.value;
+  var bookMarkContainer = document.getElementById("right-side");
+  var bookMarks = document.createElement("div");
 
-function addCard()
+  bookMarkContainer.insertBefore(bookMarks, bookMarkContainer.firstChild);
+  return bookMarks.innerHTML = `<div class="web-title-cards">
+    <h2>${title}</h2><hr>
+    <a  href="${url}">${url}</a><hr>
+    <button class="read-button">Read</button>
+    <button class="delete-button">Delete</button>
+  </div>`;
+}
